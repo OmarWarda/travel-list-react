eb3e08eaf1445e2943f3857307509fd0be41b72a
Omar <Omarwarda01@gmail.com>
2024-07-04 17:20:31 +0300

    adding States

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import {useState} from 'react';
 const initialItems = [
   {id: 1, description: 'Passports', quantity: 2, packed: false},
   {id: 2, description: 'Socks', quantity: 12, packed: false},
-  {id: 2, description: 'Charger', quantity: 1, packed: true},
+  {id: 3, description: 'Charger', quantity: 1, packed: true},
 ];
 
 export default function App () {
@@ -20,10 +21,40 @@ function Logo () {
 }
 
 function Form () {
+  const [description, setDescription] = useState ('');
+  const [quantity, setQuantity] = useState (1);
+
+  function handleSubmit (event) {
+    event.preventDefault (); // to prevent reloading the page
+
+    if (!description) return;
+    const newItem = {description, quantity, packed: false, id: Date.now ()};
+    // Now we need to pass this newItem to component PackagingList
+    console.log (newItem);
+
+    // Return the quantity and description to the default values
+    setQuantity (1);
+    setDescription ('');
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 🔥</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={e => setQuantity (Number (e.target.value))}
+      >
+        {Array.from ({length: 20}, (_, i) => i + 1).map (num => (
+          <option value={num} key={num}>{num}</option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={e => setDescription (e.target.value)} // e.target.value --> gives us what is written inside the input field
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +62,7 @@ function PackagingList () {
   return (
     <div className="list">
       <ul>
-        {initialItems.map (item => <Item itemProp={item} />)}
+        {initialItems.map (item => <Item itemProp={item} key={item.id} />)}
       </ul>
     </div>
   );
